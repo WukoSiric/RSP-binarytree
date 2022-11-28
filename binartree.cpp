d6f720627a731eadcf64517620f87ba0bf72de6f
Luke Stainer <lstainer2002@gmail.com>
2022-11-28 16:49:07 +1030

    Constructor ccreated

--- binartree.cpp
+++ binartree.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
 
+class BinaryTree {
+    int item; 
+    BinaryTree * parent = NULL;
+    BinaryTree * left = NULL;
+    BinaryTree * right = NULL;
+public:
+    BinaryTree(int item) {
+        this->item = item;
+    }
+};
+
 int main() {
 
     return 0;
